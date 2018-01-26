ca555cbed9d2e9696f180148bdb7edcd2b9ce66c
nhathan <ilove1q@yahoo.com.vn>
2018-01-26 08:51:48 +0100

    Add other commands

--- gui/js/subprocess.js
+++ gui/js/subprocess.js
@@ -18,6 +18,39 @@ let api = require(process.cwd() + '/api');
 process.on('message', (msg) => {
     let args = msg.args;
     switch (msg.command) {
+        case 'dirextract':
+            api.dirextract(args.src, args.outdir)
+                .then(() => {
+                    process.send(log + 'Done!');
+                    process.send(true);
+                })
+                .catch((error) => {
+                    process.send(err + error);
+                    process.send(false);
+                });
+            break;
+        case 'soundfix':
+            api.soundfix(args.src, args.ulpath)
+                .then(() => {
+                    process.send(log + 'Done!');
+                    process.send(true);
+                })
+                .catch((error) => {
+                    process.send(err + error);
+                    process.send(false);
+                });
+            break;
+        case 'xcpextract':
+            api.xcpextract(args.src, args.outdir)
+                .then(() => {
+                    process.send(log + 'Done!');
+                    process.send(true);
+                })
+                .catch((error) => {
+                    process.send(err + error);
+                    process.send(false);
+                });
+            break;
         case 'extract':
             api.extract(args.src, args.outdir)
                 .then(() => {
